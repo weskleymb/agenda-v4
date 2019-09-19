1c571a1679e45184bf8ea836d0b83cf9358bfce9
Weskley Bezerra <weskleymb@hotmail.com>
2019-09-19 20:52:32 -0300

    primeiro commit

--- src/main/java/br/senac/rn/agenda/controller/ContatoController.java
+++ src/main/java/br/senac/rn/agenda/controller/ContatoController.java
@@ -6,25 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/contatos")
 public class ContatoController {
 
     @Autowired
     private ContatoService service;
 
-    @GetMapping("/")
-    public String helloWorld() {
-        return "index";
-    }
-
-    @GetMapping("/contatos")
+    @GetMapping
     public String listaTodos(Model model) {
+        Contato contato = new Contato();
+        model.addAttribute("contato", contato);
         List<Contato> contatos = service.listaTodos();
         model.addAttribute("contatos", contatos);
         return "contatos";
+    }
+
+    @PostMapping
+    public String salva(Contato contato) {
+        service.salva(contato);
+        return "redirect:/contatos";
     }
 
 }
