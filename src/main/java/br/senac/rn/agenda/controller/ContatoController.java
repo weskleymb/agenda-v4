fed4d11afba1f0cdbdc6bcc9d23e2f6525bac5a3
Weskley Bezerra <weskleymb@hotmail.com>
2019-09-23 18:03:56 -0300

    primeiro commit

--- src/main/java/br/senac/rn/agenda/controller/ContatoController.java
+++ src/main/java/br/senac/rn/agenda/controller/ContatoController.java
@@ -24,6 +24,7 @@ public class ContatoController {
         model.addAttribute("contato", contato);
         List<Contato> contatos = service.listaTodos();
         model.addAttribute("contatos", contatos);
+        System.out.println(contato);
         return "contatos";
     }
 
--- src/main/java/br/senac/rn/agenda/model/Contato.java
+++ src/main/java/br/senac/rn/agenda/model/Contato.java
@@ -1,10 +1,17 @@
 package br.senac.rn.agenda.model;
 
+import lombok.*;
 import javax.persistence.*;
-import java.util.Objects;
+
 
 @Entity
 @Table(name = "tb_contatos")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class Contato {
 
     @Id
@@ -18,40 +25,4 @@ public class Contato {
     @Column(name = "con_fone", nullable = false)
     private String fone;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getFone() {
-        return fone;
-    }
-
-    public void setFone(String fone) {
-        this.fone = fone;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Contato contato = (Contato) o;
-        return id.equals(contato.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
