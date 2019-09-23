cc7441dc32c0ae7e7db3bf6aed5894c853718c58
Weskley Bezerra <weskleymb@hotmail.com>
2019-09-23 18:10:59 -0300

    primeiro commit

--- src/main/java/br/senac/rn/agenda/model/Contato.java
+++ src/main/java/br/senac/rn/agenda/model/Contato.java
@@ -1,8 +1,18 @@
 package br.senac.rn.agenda.model;
 
-import lombok.*;
-import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_contatos")
