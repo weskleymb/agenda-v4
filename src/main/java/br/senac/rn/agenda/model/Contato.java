84aaaeeb5173d6545d6d0c9603021148b2fa6cc5
Weskley Bezerra <weskleymb@hotmail.com>
2019-10-09 19:15:09 -0300

    producao

--- src/main/java/br/senac/rn/agenda/model/Contato.java
+++ src/main/java/br/senac/rn/agenda/model/Contato.java
@@ -1,34 +1,22 @@
 package br.senac.rn.agenda.model;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
+@Data
 @Entity
 @Table(name = "tb_contatos")
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@EqualsAndHashCode
-@ToString
 public class Contato {
 
     @Id
     @Column(name = "con_id")
-    @SequenceGenerator(name="tb_contatos_id_sequence", sequenceName="tb_contatos_id_sequence", allocationSize=1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tb_contatos_id_sequence")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "con_nome", nullable = false)
