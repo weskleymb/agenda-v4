b28be5d2470725ffb1f8798a59e1a878eb7c40c0
Weskley Bezerra <weskleymb@hotmail.com>
2019-10-10 19:45:54 -0300

    heroku

--- src/main/java/br/senac/rn/agenda/config/AgendaApplicationConfig.java
+++ src/main/java/br/senac/rn/agenda/config/AgendaApplicationConfig.java
@@ -5,13 +5,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AgendaConfig implements WebMvcConfigurer {
+public class AgendaApplicationConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET");
+                .allowedMethods("GET", "POST");
     }
 
 }
